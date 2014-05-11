100750d88fe651f18c7464a0172793b31bac9e11
ywzhaiqi <ywzhaiqi@gmail.com>
2014-05-11 15:52:01 +0800

    更新 verycd补全下载链接 到最新版

---
+++ verycdShowLink/README.md
@@ -0,0 +1,40 @@
+### 介绍 Introduction
+
+在 verycd 页面直接显示下载链接，可通过多个站点获取，防止失效。
+
+Chrome 需要安装 tampermonkey
+
+### 更新 Updates
+
+- Ver1.5.2 2014-05-11。
+	- 更改 @downloadURL 到 greasyfork，修正逛大街的获取等。
+- Ver1.5.1 2013-08-20。
+	- 图书增加豆瓣搜索链接
+- Ver1.5.0 2013-04-09。
+	- 逛大街改版失效，已经切换至 SimpleCD，简化程序运行的流程。
+- Ver1.4.0 2013-03-16。
+	- 修复Chrome下解析HTML
+- Ver1.3.9.3 2013-03-10。
+	- 修复ed2kers搜索中包含 ' 的bug
+- Ver1.3.9.2 2013-03-08。
+	- 修复simplecd、ed2kers个别的搜索bug
+- Ver1.3.8 2013-03-01。
+	- 大幅完善了脚本，已经基本完善了，除非3个网站都失效，否则应该能获取到链接。
+	- 完善了多选，复制到剪贴板。
+- Ver1.3.6 2013-02-26。
+	- 完善多个网站的获取内容，能跳过搜索链接得到直接链接
+- Ver1.3.5 2013-02-26。
+	- 增加多个站点搜索链接
+- Ver1.3.3 2013-02-20。
+	- 去除 dbank（华为网盘）功能，可以用 [DBank Linker][0] 脚本替代
+- Ver1.3.0 2013-02-17。
+	- 增加：去除华为网盘插件下载
+
+### 参考 Reference
+
+* Crack Url Wait Code Login
+http://userscripts.org/scripts/show/153190
+* verycdfetch
+http://userscripts.org/scripts/show/153429
+
+[0]: http://userscripts.org/scripts/show/116879
--- verycdShowLink/verycd@ywzhaiqi@gmailcom.user.js
+++ verycdShowLink/verycd@ywzhaiqi@gmailcom.user.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @id             verycd@ywzhaiqi@gmail.com
 // @name           verycd补全下载链接
-// @version        1.5.0
+// @version        1.5.2
 // @author         ywzhaiqi@gmail.com
-// @description    在verycd页面直接显示下载链接
-// @downloadURL    http://userscripts.org/scripts/source/158764.user.js
-// @updateURL      http://userscripts.org/scripts/source/158764.meta.js
+// @description    在 verycd 页面直接显示下载链接，可通过多个站点获取，防止失效。
+// downloadURL     http://userscripts.org/scripts/source/158764.user.js
+// updateURL       http://userscripts.org/scripts/source/158764.meta.js
+
+// @homepageURL    https://greasyfork.org/scripts/301/
 // @include        http://www.verycd.com/topics/*
 // @require        http://code.jquery.com/jquery-1.9.1.min.js
-// @require        https://raw.github.com/olado/doT/master/doT.min.js
+// @namespace https://greasyfork.org/users/145
 // ==/UserScript==
 
 var debug = false;
 
-var xbug = debug ? console.debug : function(){},
-    xlog = debug ? console.log : function(){};
+var xbug = debug ? console.debug.bind(console) : function(){},
+    xlog = debug ? console.log.bind(console) : function(){};
 
 //------------------------------------ 一些辅助函数  ---------------------------------------------------
 $.extend({
@@ -22,6 +24,15 @@ $.extend({
         return doT.template(template).apply(null,[data]);
     }
 });
+
+/* Laura Doktorova https://github.com/olado/doT */
+(function(){function o(){var a={"&":"&#38;","<":"&#60;",">":"&#62;",'"':"&#34;","'":"&#39;","/":"&#47;"},b=/&(?!#?\w+;)|<|>|"|'|\//g;return function(){return this?this.replace(b,function(c){return a[c]||c}):this}}function p(a,b,c){return(typeof b==="string"?b:b.toString()).replace(a.define||i,function(l,e,f,g){if(e.indexOf("def.")===0)e=e.substring(4);if(!(e in c))if(f===":"){a.defineParams&&g.replace(a.defineParams,function(n,h,d){c[e]={arg:h,text:d}});e in c||(c[e]=g)}else(new Function("def","def['"+
+e+"']="+g))(c);return""}).replace(a.use||i,function(l,e){if(a.useParams)e=e.replace(a.useParams,function(g,n,h,d){if(c[h]&&c[h].arg&&d){g=(h+":"+d).replace(/'|\\/g,"_");c.__exp=c.__exp||{};c.__exp[g]=c[h].text.replace(RegExp("(^|[^\\w$])"+c[h].arg+"([^\\w$])","g"),"$1"+d+"$2");return n+"def.__exp['"+g+"']"}});var f=(new Function("def","return "+e))(c);return f?p(a,f,c):f})}function m(a){return a.replace(/\\('|\\)/g,"$1").replace(/[\r\t\n]/g," ")}var j={version:"1.0.1",templateSettings:{evaluate:/\{\{([\s\S]+?(\}?)+)\}\}/g,
+interpolate:/\{\{=([\s\S]+?)\}\}/g,encode:/\{\{!([\s\S]+?)\}\}/g,use:/\{\{#([\s\S]+?)\}\}/g,useParams:/(^|[^\w$])def(?:\.|\[[\'\"])([\w$\.]+)(?:[\'\"]\])?\s*\:\s*([\w$\.]+|\"[^\"]+\"|\'[^\']+\'|\{[^\}]+\})/g,define:/\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,defineParams:/^\s*([\w$]+):([\s\S]+)/,conditional:/\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,iterate:/\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,varname:"it",strip:true,append:true,selfcontained:false},template:undefined,
+compile:undefined},q;if(typeof module!=="undefined"&&module.exports)module.exports=j;else if(typeof define==="function"&&define.amd)define(function(){return j});else{q=function(){return this||(0,eval)("this")}();q.doT=j}String.prototype.encodeHTML=o();var r={append:{start:"'+(",end:")+'",endencode:"||'').toString().encodeHTML()+'"},split:{start:"';out+=(",end:");out+='",endencode:"||'').toString().encodeHTML();out+='"}},i=/$^/;j.template=function(a,b,c){b=b||j.templateSettings;var l=b.append?r.append:
+r.split,e,f=0,g;a=b.use||b.define?p(b,a,c||{}):a;a=("var out='"+(b.strip?a.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g," ").replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,""):a).replace(/'|\\/g,"\\$&").replace(b.interpolate||i,function(h,d){return l.start+m(d)+l.end}).replace(b.encode||i,function(h,d){e=true;return l.start+m(d)+l.endencode}).replace(b.conditional||i,function(h,d,k){return d?k?"';}else if("+m(k)+"){out+='":"';}else{out+='":k?"';if("+m(k)+"){out+='":"';}out+='"}).replace(b.iterate||i,function(h,
+d,k,s){if(!d)return"';} } out+='";f+=1;g=s||"i"+f;d=m(d);return"';var arr"+f+"="+d+";if(arr"+f+"){var "+k+","+g+"=-1,l"+f+"=arr"+f+".length-1;while("+g+"<l"+f+"){"+k+"=arr"+f+"["+g+"+=1];out+='"}).replace(b.evaluate||i,function(h,d){return"';"+m(d)+"out+='"})+"';return out;").replace(/\n/g,"\\n").replace(/\t/g,"\\t").replace(/\r/g,"\\r").replace(/(\s|;|\}|^|\{)out\+='';/g,"$1").replace(/\+''/g,"").replace(/(\s|;|\}|^|\{)out\+=''\+/g,"$1out+=");if(e&&b.selfcontained)a="String.prototype.encodeHTML=("+
+o.toString()+"());"+a;try{return new Function(b.varname,a)}catch(n){typeof console!=="undefined"&&console.log("Could not create a template function: "+a);throw n;}};j.compile=function(a,b){return j.template(a,null,b)}})();
 
 
 Function.prototype.getMultiLine = function() {  // 多行String
@@ -31,7 +42,6 @@ Function.prototype.getMultiLine = function() {  // 多行String
 }
 
 function getDoc(url, callback, data){
-  
     GM_xmlhttpRequest({
         method: data ? 'POST' : 'GET',
         url: url,
@@ -55,6 +65,29 @@ function getDoc(url, callback, data){
     });
 }
 
+function byte2Str(inp) {
+    var f = parseFloat(inp),
+        unit = ['字节', 'KB', 'MB', 'GB', 'TB'],
+        fUnit = 0;
+    while (f > 1126) {
+        f /= 1024;
+        fUnit += 1;
+    }
+    return f.toFixed(2).toString() + ' ' + (unit[fUnit] || '未知单位');
+}
+
+function ed2kLink(aHref) {
+    var ed2kArr = aHref.substr(0, aHref.length - 2).substr(13).split('|');
+    return ({
+        url: aHref,
+        name: decodeURIComponent(ed2kArr[0]),
+        size: parseInt(ed2kArr[1]),
+        sizs: byte2Str(ed2kArr[1]),
+        hash: ed2kArr[2],
+        extr: ed2kArr[3]
+    });
+}
+
 
 var VERYCD = {
     root: $('#iptcomED2K'),
@@ -62,7 +95,6 @@ var VERYCD = {
     curSitePos: 0,
 
     init: function() {
-
         if(!this.root){
             console.error('root is null');
             return;
@@ -76,11 +108,9 @@ var VERYCD = {
 
             this.get(VERYCD.curSitePos);
         }
-
     },
 
     display_sites: function(){
-
         // 插入其他站点的链接
         $('#iptcomED2K').before(
             $.tmpl(VERYCD.tpl_sites, VERYCD)
@@ -88,7 +118,6 @@ var VERYCD = {
     },
 
     display_content: function(ed2ks){
-
         // 添加到 网页中
         $('#iptcomED2K').html(
             $.tmpl(VERYCD.tpl_content, ed2ks)
@@ -96,14 +125,13 @@ var VERYCD = {
         // 计算总的大小
         unsafeWindow.em_size('EM4e759f9673554');
     },
-
     tpl_sites: function(){  // 插入的头部模板
         /*
         <table id="other_links">
             <tr>
                 {{ for(var i = 0, l = it.sites.length; i < l; i++){ }}
                 <td>
-                    <a  id="{{=it.sites[i].id}}" href="{{=it.sites[i].url}}" target="_blank" style="background: #C00000; color: white;
+                    <a  id="{{=it.sites[i].id}}" href="{{=it.sites[i].url}}" target="_blank" style="
                         height: 20px; text-align: center; vertical-align: middle; padding: 2px 16px; cursor: pointer;">
                     显示资源: {{=it.sites[i].name}}
                     </a>
@@ -113,7 +141,6 @@ var VERYCD = {
         </table>
         */
     }.getMultiLine(),
-
     tpl_content: function(){  // 插入的内容模板
         /*
         <div class="emuletop">电驴资源</div>
@@ -124,9 +151,9 @@ var VERYCD = {
                         <td>
                             <input value="{{=it[i].url}}" name="EM4e759f9673554" onclick="em_size('EM4e759f9673554');" checked="checked"
                                 type="checkbox" style="cursor: pointer;" class="forminput">
-                            <a href="{{=it[i].url}}" ed2k="{{=it[i].url}}">{{=it[i].title}}</a>
+                            <a href="{{=it[i].url}}" ed2k="{{=it[i].url}}">{{=it[i].name}}</a>
                         </td>
-                        <td align="center">{{=it[i].sizeStr}}</td>
+                        <td align="center">{{=it[i].sizs}}</td>
                     </tr>
                     {{ } }}
                     <tr><td align="left" class="post2"><input type="checkbox" checked="" class="forminput" id="checkall_EM4e759f9673554" onclick="checkAll('EM4e759f9673554',this.checked)"> <label for="checkall_EM4e759f9673554">全选</label> <input type="button" class="button downall" value="下载选中的文件" onclick="download('EM4e759f9673554',0,1)"> <span id="updateflashEM4e759f9673554"><object width="136" height="20" align="middle" class="copyflash" id="ed2kcopy_EM4e759f9673554" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" onload="em_size(&quot;EM4e759f9673554&quot;);"><param value="always" name="allowScriptAccess"><param value="false" name="allowFullScreen"><param value="/cp2flash.swf" name="movie"><param value="high" name="quality"><param value="#ffffff" name="bgcolor"><param value="transparent" name="wmode"><param value="flashID=EM4e759f9673554" name="FlashVars"><embed width="136" height="20" align="middle" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" flashvars="flashID=EM4e759f9673554" allowfullscreen="false" allowscriptaccess="always" name="ed2kcopy_EM4e759f9673554" bgcolor="#ffffff" quality="high" wmode="transparent" src="/cp2flash.swf"></object></span>' </td><td align="center" id="size_EM4e759f9673554" class="emulesize post2"></td></tr>
@@ -137,37 +164,30 @@ var VERYCD = {
     }.getMultiLine(),
 
     get_real_url: function(site, callback){  // 跳过搜索链接，取得直接链接
-
-        xbug("Get Real URL: " + site.url);
-
+        xbug("Start Get Real URL: " + site.url);
         getDoc(site.url, function(doc){
             var find_url = $(site.search_selector, doc).attr('href');
-
             $site = $('#' + site.id);
+
             if(find_url){
                 site.real_url = site.BASE_URL + find_url;
                 $site.attr('href', site.real_url);
 
                 callback(site.real_url);
             }else{   // 如果没找到链接则取下一个站点
+                xbug('没有找到 real_url，继续获取下一个站点');
                 VERYCD.get(++VERYCD.curSitePos);
 
                 $site.text(site.name + '（无资源）')
-                    .css('background', '#77787b');
+                     .css('background', '#77787b');
             }
-
         });
     },
-
     get_dlink_page: function(url){
-
-        xbug("Get dlink Page: " + url);
-
+        xbug("Start Get dlink Page: " + url);
         getDoc(url, function(doc){
             if(doc){
-
                 xbug("Current Site Pos: " + VERYCD.curSitePos);
-
                 var site = VERYCD.sites[VERYCD.curSitePos],
                     pageProcessor = site.pageProcessor ? site.pageProcessor : VERYCD.commondPageProcessor;
                 pageProcessor(doc, site);
@@ -176,9 +196,7 @@ var VERYCD = {
             }
         });
     },
-
     get: function(sitePos){
-
         var sites = this.sites;
 
         if(sitePos >= sites.length) return;
@@ -187,19 +205,15 @@ var VERYCD = {
 
         if(site.search_selector){  // 如果需要搜索, 则获取真实地址
             this.get_real_url(site, this.get_dlink_page);
-        }else{  
+        }else{
             this.get_dlink_page(site.url);
         }
     },
-
     commondPageProcessor: function(doc){  // 通用的页面处理
         var data = [];
         $(doc).find('a[href^="ed2k"]').each(function(i, el){
-            data[i] = {
-                url: $(this).attr('href').replace('[www.ed2kers.com]', ''),
-                title: $(this).text(),
-                sizeStr: $(this).parent().next().text()
-            };
+            var url = $(this).attr('href').replace('[www.ed2kers.com]', '');
+            data[i] = ed2kLink(url)
         });
 
         VERYCD.display_content(data);
@@ -209,13 +223,17 @@ var VERYCD = {
 
 // 其他站点的配置
 VERYCD.sites = [
-
+    {   name: '逛大街（默认）',
+        id: 'site_gdajie',
+        BASE_URL: '',
+        url: location.href.replace('www.verycd.com', 'www.verycd.gdajie.com'),
+        search_selector: "#emuleFile a[href^='http://www.verycd.gdajie.com']",
+    },
     {
         name: "SimpleCD",
         id: 'site_simplecd',
         BASE_URL: "http://simplecd.me",
         url: "http://simplecd.me/search/entry/?query=" + VERYCD.title.replace(/\/|-|&/g, " "),
-        real_url: null,
         search_selector: ".entry-list a:contains('" + VERYCD.title + "')",
         pageProcessor: function(doc, site){
             var data = [];
@@ -223,43 +241,26 @@ VERYCD.sites = [
             $(doc).find('.emulemain input[name="selectemule"]').each(function(i, el){
                 var rid = $(this).attr('value');
                 ids[i] = "rid=" + rid;
-                // 获取title、大小
-                data[i] = {
-                    title: $(this).next().text(),
-                    sizeStr: $(this).parent().siblings('td:last').text()
-                };
             });
+
             var url = site.BASE_URL + '/download/?mode=copy&' + ids.join('&');
-
             xbug("Get SimpleCD: " + url);
-
             getDoc(url, function(doc){
                 $('td:contains("ed2k://")', doc).each(function(i, el){
-                    data[i].url = $(this).text();
+                    var url = $(this).text();
+                    data[i] = ed2kLink(url);
                 });
                 VERYCD.display_content(data);
             });
         }
     },
-
-    {   name: 'ed2kers',
+    {   
+        name: 'ed2kers',
         id: 'site_ed2kers',
         BASE_URL: 'http://www.ed2kers.com',
         url: "http://www.ed2kers.com/index.php/search/index?c=0&keyword=" + VERYCD.title.replace(/'/g, " "),
-        real_url: null,
         search_selector: ".techdes a:contains('" + VERYCD.title.replace(/&/g, "&amp;") + "')"
     }
-    
-    /* 2013年4月9日 改版后不能直接获取地址 
-    {   name: '逛大街（默认）',
-        id: 'site_gdajie',
-        BASE_URL: 'http://www.verycd.gdajie.com',
-        url: location.href.replace('www.verycd.com', 'www.verycd.gdajie.com'),
-        pageProcessor: function(doc, site){
-            // #detail a
-        }
-    },
-    */
 
     // {   name: 'VeryCD Fetch', host: 'verycdfetch.duapp.com',  //2013-2-10 测试失效
     //     url: location.href.replace('www.verycd.com', 'verycdfetch.duapp.com'),
@@ -271,9 +272,36 @@ VERYCD.sites = [
     //     }}
 ];
 
+var Douban = {
+    searchURLs: {
+        "图书": "http://book.douban.com/subject_search?search_text=%s&cat=1001"
+    },
+    init: function(){
+        var type = $("#whereru>a:eq(1)").text();
+        var searchURL = this.searchURLs[type];
+        if(!searchURL) return;
+
+        var title = this.getTitle();
+        var url = searchURL.replace("%s", encodeURIComponent(title));
+
+        var link = $("<a>豆瓣" + type + "搜索</a>").attr({
+            href: url,
+            target: "_blank",
+            style: "height: 20px; text-align: center; vertical-align: middle; padding: 2px 16px; cursor: pointer;"
+        });
+
+        $("<td>").append(link).appendTo('#other_links tr:eq(0)')
+    },
+    getTitle: function(){
+        var title = $("#topicstitle").text();
+        var m = title.match(/《(.*?)》/);
+
+        return m[1] || title;
+    }
+};
+
+
+
 VERYCD.init();
 
-var debug = false;
-if(debug){
-    unsafeWindow.VCD = VERYCD;
-}
+Douban.init();
