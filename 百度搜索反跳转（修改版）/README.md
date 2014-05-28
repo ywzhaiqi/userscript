a8a40bbf844d07ad93cafb5cc649ba88ff4ad66d
ywzhaiqi <ywzhaiqi@gmail.com>
2014-05-28 16:49:42 +0800

    放弃重定向方法，改用监视内容部分变化的方法。增加 greasyfork 更新链接。

--- 百度搜索反跳转（修改版）/README.md
+++ 百度搜索反跳转（修改版）/README.md
@@ -1,7 +1,11 @@
 百度搜索反跳转（修改版）
 ===================
 
-改自 [百度搜索反跳转](http://userscripts.org:8080/scripts/show/161812)，增加了翻页脚本的支持、#wd= 页面的重定向。
+改自 [百度搜索反跳转](http://userscripts.org:8080/scripts/show/161812)，增加了翻页脚本的支持、不刷新页面的支持。
+
+[greasyfork 地址](https://greasyfork.org/scripts/1771)，[github 地址](https://github.com/ywzhaiqi/userscript/tree/master/%E7%99%BE%E5%BA%A6%E6%90%9C%E7%B4%A2%E5%8F%8D%E8%B7%B3%E8%BD%AC%EF%BC%88%E4%BF%AE%E6%94%B9%E7%89%88%EF%BC%89)
+
+### 旧的说明（已不准确）
 
 1. 下一页的支持有2种方式（设置在脚本内 checkNextPageMethod，默认为 第一种）
 
--- 百度搜索反跳转（修改版）/百度搜索反跳转（修改版）.user.js
+++ 百度搜索反跳转（修改版）/百度搜索反跳转（修改版）.user.js
@@ -2,116 +2,161 @@
 // @name         Baidu 百度搜索反跳转（修改版）
 // @namespace    noe132
 // @modified     ywzhaiqi
+// @description  改自 百度搜索反跳转，增加了翻页脚本的支持、不刷新页面的支持。
 // @include      http://www.baidu.com/*
+// @homepageURL  https://greasyfork.org/scripts/1771/
+// @updateURL    https://greasyfork.org/scripts/1771/code.meta.js
+// @downloadURL  https://greasyfork.org/scripts/1771/code.user.js
+
 // updateURL     https://userscripts.org/scripts/source/161812.meta.js
 // downloadURL   https://userscripts.org/scripts/source/161812.user.js
-// @icon	     http://tb.himg.baidu.com/sys/portrait/item/d4346e6f65313332ac06
-// @version      1.2.1
-// @run-at       document-start
-// @note         2014-5-24，添加支持翻页脚本
+// @icon         http://tb.himg.baidu.com/sys/portrait/item/d4346e6f65313332ac06
+// @version      2014.5.28 1.2.1 
+// @run-at       document-end
+// @note         2014-5-28，增加对百度不刷新页面的支持
+// @note         2014-5-24，增加对翻页脚本的支持
 // ==/UserScript==
 
-// 下一页的支持有2种方式
+// 下一页的支持有2种方式（已不准确）
 //     0 对 Super_preloaderPlus_one、AutoPagerize、uAutoPagerize、 BaiduMonkeyW 脚本的支，准确、资源消耗相对更小
 //     1 对所有脚本都适用，但不准确、消耗相对较大。如果非上面的几个脚本，建议用这个。
+
 var checkNextPageMethod = 0;
 
 function decode(url,target){
-	GM_xmlhttpRequest({
-		method: "GET",
-		url: 'http://noe132.duapp.com/baidu2.php?url=' + url,
-		onload: function(response){
-			var newUrl = response.responseText;
-			if (!newUrl || newUrl.indexOf('http') != 0) {
-				return;
-			}
-			// console.log(newUrl)
-			target.setAttribute('href', newUrl);
-		}
-	});
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: 'http://noe132.duapp.com/baidu2.php?url=' + url,
+        onload: function(response){
+            var newUrl = response.responseText;
+            if (!newUrl || newUrl.indexOf('http') != 0) {
+                return;
+            }
+            // console.log(newUrl)
+            target.setAttribute('href', newUrl);
+        }
+    });
 }
 
 function checkDocument(doc) {
-	if (!doc) doc = document;
-	var links = doc.querySelectorAll('a[href^="http://www.baidu.com/link?url="]');
-	if (!links) return;
-	[].forEach.call(links, function(link){
-		// console.log('decode url: ', link.href)
-		decode(link.href, link);
-	});
+    if (!doc) doc = document;
+    var links = doc.querySelectorAll('a[href^="http://www.baidu.com/link?url="]');
+    if (!links) return;
+    [].forEach.call(links, function(link){
+        // console.log('decode url: ', link.href)
+        decode(link.href, link);
+    });
 }
 
 function nextPageLoaded() {
-	// AutoPagerizeの最後の区切り以降のRangeを取得
-	var sep = document.querySelectorAll('.autopagerize_page_separator, .autopagerize_page_info' + ', .sp-separator'
-		+ ', p[id="page"]');
-	sep = sep[sep.length-1];
-	if (!sep) return;
-	var range = document.createRange();
-	if (sep.parentNode.localName == 'td') {
-	    range.setStartAfter(sep.parentNode.parentNode);
-	    range.setEndAfter(sep.parentNode.parentNode.parentNode);
-	} else {
-	    range.setStartAfter(sep);
-	    range.setEndAfter(sep.parentNode.lastChild);
-	}
+    // AutoPagerizeの最後の区切り以降のRangeを取得
+    var sep = document.querySelectorAll('.autopagerize_page_separator, .autopagerize_page_info' + ', .sp-separator'
+        + ', p[id="page"]');
+    sep = sep[sep.length-1];
+    if (!sep) return;
+    var range = document.createRange();
+    if (sep.parentNode.localName == 'td') {
+        range.setStartAfter(sep.parentNode.parentNode);
+        range.setEndAfter(sep.parentNode.parentNode.parentNode);
+    } else {
+        range.setStartAfter(sep);
+        range.setEndAfter(sep.parentNode.lastChild);
+    }
 
-	checkDocument(range.commonAncestorContainer);
+    checkDocument(range.commonAncestorContainer);
 }
 
-function addObserver() {
-	// 创建观察者对象
-	var observer = new MutationObserver(function(mutations){
-		var needAdd = false;
-		for (var i = mutations.length - 1; i >= 0; i--) {
-			if (mutations[i].addedNodes.length) {
-				needAdd = true;
-				break;
-			}
-		}
+function addNextPageObserver() {
+    // 创建观察者对象
+    var observer = new MutationObserver(function(mutations){
+        var needAdd = false;
+        for (var i = mutations.length - 1; i >= 0; i--) {
+            if (mutations[i].addedNodes.length) {
+                needAdd = true;
+                break;
+            }
+        }
 
-	    if (needAdd) {
-	        checkDocument();
-	    }
-	});
-	observer.observe(document.body, {childList: true, subtree: true});
+        if (needAdd) {
+            checkDocument();
+        }
+    });
+    observer.observe(document.body, {childList: true, subtree: true});
 }
 
-function onHashchange() {
-	window.removeEventListener('hashchange', onHashchange, false);
+function addMutationObserver(selector, callback) {
+    var watch = document.querySelector(selector);
+    if (!watch) return;
 
-	var url = window.location.href;
-	if (url.indexOf("http://www.baidu.com/#") >= 0) {
-		console.log("redirect to s?");
-		window.location.href = url.replace("http://www.baidu.com/#", "http://www.baidu.com/s?");
-		return;
-	}
+    var observer = new MutationObserver(function(mutations){
+        var nodeAdded = mutations.some(function(x){ return x.addedNodes.length > 0; });
+        if (nodeAdded) {
+            // observer.disconnect();
+            callback();
+        }
+    });
+    observer.observe(watch, {childList: true});
 }
 
-function run() {
-	// 百度搜索 /#wd= 页面重定向到baidu?
-	var url = window.location.href;
-	if (url.indexOf("http://www.baidu.com/#") >= 0) {
-		console.log("redirect to s?");
-		window.location.href = url.replace("http://www.baidu.com/#", "http://www.baidu.com/s?");
-		return;
-	} else if (url == 'http://www.baidu.com/') {
-		console.log('add hashchange Event');
-		window.addEventListener('hashchange', onHashchange, false);
-		return;
-	}
 
-	addEventListener('DOMContentLoaded', function(){
-		checkDocument();
 
-		if (!checkNextPageMethod) {
-			document.addEventListener("GM_AutoPagerizeNextPageLoaded", nextPageLoaded, false);
-			document.addEventListener("Super_preloaderPageLoaded", nextPageLoaded, false);
-			document.addEventListener("bm_NextPageLoaded", nextPageLoaded, false);
-		} else {
-			addObserver();
-		}
-	}, false);
-}
+var url = location.href;
 
-run()
+checkDocument();
+
+// 添加下一页和不刷新页面的支持
+addMutationObserver('#wrapper_wrapper', function(){
+    // console.log('元素被添加')
+    checkDocument();
+});
+
+
+// // 添加下一页的支持
+// if (!checkNextPageMethod) {
+//  document.addEventListener("GM_AutoPagerizeNextPageLoaded", nextPageLoaded, false);
+//  document.addEventListener("Super_preloaderPageLoaded", nextPageLoaded, false);
+//  document.addEventListener("bm_NextPageLoaded", nextPageLoaded, false);
+// } else {
+//  addNextPageObserver();
+// }
+
+// // 添加不刷新页面的支持
+// if (url.match(/^https?:\/\/www\.baidu\.com\/[^#]*($|#wd=)/)) {
+//  addMutationObserver('#wrapper_wrapper', function(){
+//      // console.log('元素被添加')
+//      checkDocument();
+//  });
+// }
+
+// function onHashchange() {
+//  window.removeEventListener('hashchange', onHashchange, false);
+
+//  var url = window.location.href;
+//  if (url.indexOf("http://www.baidu.com/#") >= 0) {
+//      console.log("redirect to s?");
+//      window.location.href = url.replace("http://www.baidu.com/#", "http://www.baidu.com/s?");
+//      return;
+//  }
+// }
+
+// function run() {
+//  // 百度搜索 /#wd= 页面重定向到baidu?
+//  var url = window.location.href;
+//  if (url.indexOf("http://www.baidu.com/#") >= 0) {
+//      console.log("redirect to s?");
+//      window.location.href = url.replace("http://www.baidu.com/#", "http://www.baidu.com/s?");
+//      return;
+//  } else if (url == 'http://www.baidu.com/') {
+//      console.log('add hashchange Event');
+//      window.addEventListener('hashchange', onHashchange, false);
+//      return;
+//  }
+
+//  addEventListener('DOMContentLoaded', function(){
+//      checkDocument();
+
+        
+//  }, false);
+// }
+
+// run()
