ea0c38bebf7a075c4891bc2ea59778c105e99bf8
ywzhaiqi <ywzhaiqi@gmail.com>
2014-06-17 16:55:17 +0800

    改用 cookie 方式，来自广告屏蔽大师

--- .test/RedirectBaidu.user.js
+++ .test/RedirectBaidu.user.js
@@ -1,29 +1,10 @@
 // ==UserScript==
 // @name           RedirectBaidu
-// @version        2014.06.08
+// @version        2014.06.16
 // @namespace      https://github.com/ywzhaiqi
 // @author         ywzhaiqi
 // @description    百度搜索重定向到 s?wd=
 // @include        http://www.baidu.com/*
 // ==/UserScript==
 
-
-// 百度搜索 /#wd= 页面重定向到baidu?
- var url = window.location.href;
-
- if (url.indexOf('#wd=')) {
-    redirect();
-} else if (url == 'http://www.baidu.com/') {
-    console.log('add hashchange Event');
-    window.addEventListener('hashchange', function(){
-        // window.removeEventListener('hashchange', onHashchange, false);
-        redirect();
-    }, false);
-}
-
-function redirect() {
-    if (url.indexOf('#wd=')) {
-        console.log('redirect to s?');
-        window.location.href = url.replace(/^(https?):\/\/www.baidu.com\/[^#]*#wd=/, '$1://www.baidu.com/s?wd=');
-    }
-}
+document.cookie = "ISSW=1";
