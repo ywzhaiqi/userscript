595097f4dc7215a3044f77ff816a23a1f7664ac6
ywzhaiqi <ywzhaiqi@gmail.com>
2014-06-24 15:29:44 +0800

    6.2.1 拆分采用 grunt 方式，规则更新
    - 修正百度搜索规则
    - 增加爱活网规则

---
+++ Super_preloaderPlus/Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+    var concatOptions = {
+        banner: '(function() {',
+        footer: '})();',
+        separator: '//----------------------------------'
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['tmp'],
+        concat: {
+            dist: {
+                options: concatOptions,
+                src: ['src/meta.js', 'src/rule.js', 'src/main.js'],
+                dest: '<%= pkg.name %>.user.js'
+            },
+        },
+        watch: {
+            files: ['src/**/*.js'],
+            tasks: ['default']
+        }
+    })
+
+
+    grunt.registerTask('split', 'split file', function () {
+        grunt.file.expand('src/*').forEach(function(path){
+            grunt.file.delete(path);
+        });
+
+        var source = grunt.file.read('super_preloaderplus_one.user.js');
+
+        // 去除首行和尾行
+        source = source.replace(/^\(function\(\).*/, '')
+            .replace(/\}\)\(\);$/, '');
+
+        var arr = source.split(/\n\/\/\-+/);
+
+        arr.forEach(function (text, index) {
+            if (index === 0) {
+                grunt.file.write('src/meta.js', text);
+            } else {
+                var match = text.match(/\n\/\/\s*(.*)/);
+                var name = match ? match[1] : ('split' + index + '.js' );
+                grunt.file.write('src/' + name, text);
+            }
+        });
+
+        // grunt.log.writeln();
+    });
+
+
+    grunt.registerTask('default', [
+        //'jshint',
+        'concat',
+    ])
+
+    grunt.registerTask('watch', ['watch'])
+
+    grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+
+}
---
+++ Super_preloaderPlus/src/meta.js
@@ -0,0 +1,45 @@
+
+// ==UserScript==
+// @name         Super_preloaderPlus_one
+// @namespace    https://github.com/ywzhaiqi
+// @description  预读+翻页..全加速你的浏览体验...
+// @author       ywzhaiqi && NLF(原作者)
+// @version      6.2.1
+// @homepageURL  https://greasyfork.org/scripts/178900/
+// @updateURL    https://greasyfork.org/scripts/293-super-preloaderplus-one/code/Super_preloaderPlus_one.meta.js
+// @downloadURL  https://greasyfork.org/scripts/293-super-preloaderplus-one/code/Super_preloaderPlus_one.user.js
+
+// homepageURL  https://userscripts.org/scripts/show/178900
+// downloadURL  https://userscripts.org/scripts/source/178900.user.js
+// updateURL    https://userscripts.org/scripts/source/178900.meta.js
+
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+
+// @include      http*
+// @exclude      http*://mail.google.com/*
+// @exclude      http*://maps.google*   
+// @exclude      http*://www.google.com/reader*
+// @exclude      http*://www.google.com/calendar*
+// @exclude      https://docs.google.com/*
+// @exclude      http*://app.yinxiang.com/*
+// @exclude      http*://www.dropbox.com/*
+// @exclude      http*://www.toodledo.com/*
+// @exclude      http://cloud.feedly.com/*
+// @exclude      http://weibo.com/*
+// @exclude      http://w.qq.com/*
+// @exclude      http://web2.qq.com/*
+// @exclude      http://openapi.qzone.qq.com/*
+// @exclude      http://*cloud.vip.xunlei.com/*
+// @exclude      http*://www.wumii.com/*
+// @exclude      http://pan.baidu.com/*
+// @exclude      http://yun.baidu.com/*
+// @exclude      http://www.cnbeta.com/*
+// @exclude      http://www.youku.com/
+// @exclude      http://v.youku.com/*
+// @exclude      http://www.iqiyi.com/*
+// ==/UserScript==
+
