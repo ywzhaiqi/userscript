107717b6f5418ab03b0d1d9cacf27a58c36fb603
ywzhaiqi <ywzhaiqi@gmail.com>
2014-09-13 11:27:13 +0800

    noNoticeTitleFlash 修正 chrome 下无效的问题

--- noNoticetitleflashOnBBS.user.js
+++ noNoticetitleflashOnBBS.user.js
@@ -1,18 +1,24 @@
 // ==UserScript==
 // @id             noNoticeTitleFlash
 // @name           No noticeTitleFlash on BBS
-// @version        1.0
+// @version        1.1
 // @namespace      https://github.com/ywzhaiqi
 // @author         ywzhaiqi
 // @description    卡饭论坛去除标题栏闪烁提醒
-// @include        http://bbs.kafan.cn/forum*
+
 // @include        http://bbs.kafan.cn/thread*
-// @include        http://bbs.ruoren.com/forum*
+// @include        http://bbs.ruoren.com/*
 // @include        http://u.kafan.cn/*
 // @include        http://bbs.feng.com/*
-// @include        http://game.ali213.net/forum*
+// @include        http://game.ali213.net/*
+// Discuz论坛列表
+// @include       /^https?:\/\/(?:www\.[^\/]+\/|[^\/]+\/(?:bbs\/)?)(?:2b\/)?(?:(?:forum)|(?:showforum)|(?:viewforum)|(?:forumdisplay))+/
+// @include       /https?:\/\/(?:www\.[^\/]+\/|[^\/]+\/(?:bbs\/)?)(?:2b\/)?(?:(?:thread)|(?:viewthread)|(?:showtopic)|(?:viewtopic))+/
+
 // @grant          none
 // @run-at         document-end
+// @noframes
 // ==/UserScript==
 
-(unsafeWindow || window).noticeTitleFlash = function() {};
+
+window.noticeTitleFlash = function(){};
