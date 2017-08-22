76761c9b2f015ad3384cd7d335bfb97c1f681a46
greatYear <ywzhaiqi@gmail.com>
2017-08-22 16:52:50 +0800

    移动位置

--- src/utils/detect.js
+++
@@ -1,8 +0,0 @@
-// 参考 https://github.com/madrobby/zepto/blob/master/src/detect.js
-
-const ua = navigator.userAgent;
-const platform = navigator.platform;
-
-export const isFirefox = ua.match(/Firefox\/([\d.]+)/);
-
-export const isChrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/);
