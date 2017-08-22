f0363cf4bb9e18beaee718bd70376112e3b64ce2
ivysrono <ivysrono@users.noreply.github.com>
2017-08-22 10:51:29 +0800

    Update super_preloaderplus_one.user.js

--- Super_preloaderPlus/super_preloaderplus_one.user.js
+++ Super_preloaderPlus/super_preloaderplus_one.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://github.com/ywzhaiqi
 // @description  预读+翻页..全加速你的浏览体验...
 // @author       ywzhaiqi && NLF(原作者)
-// @version      6.5.1
+// @version      6.5.2
 // @homepageURL  https://greasyfork.org/scripts/293-super-preloaderplus-one
 
 // @grant        GM_addStyle
@@ -14,35 +14,39 @@
 // @grant        GM_registerMenuCommand
 
 // @include      http*
-// @exclude      http*://mail.google.com/*
-// @exclude      http*://maps.google*
-// @exclude      http*://www.google.com/reader*
-// @exclude      http*://www.google.com/calendar*
-// @exclude      http*://docs.google.com/*
-// @exclude      http*://app.yinxiang.com/*
-// @exclude      http*://www.dropbox.com/*
-// @exclude      http*://www.toodledo.com/*
-// @exclude      http*://cloud.feedly.com/*
-// @exclude      http*://weibo.com/*
-// @exclude      http*://w.qq.com/*
-// @exclude      http*://web2.qq.com/*
-// @exclude      http*://openapi.qzone.qq.com/*
-// @exclude      http*://*cloud.vip.xunlei.com/*
-// @exclude      http*://www.wumii.com/*
+
 // @exclude      http*://pan.baidu.com/*
 // @exclude      http*://yun.baidu.com/*
 // @exclude      http*://www.cnbeta.com/*
-// @exclude      http*://www.youku.com/
-// @exclude      http*://v.youku.com/*
-// @exclude      http*://www.iqiyi.com/*
+// @exclude      http*://www.dropbox.com/*
 // @exclude      http*://www.duokan.com/reader/*
+// @exclude      http*://feedly.com/*
+// @exclude      http*://www.google.com/calendar*
+// @exclude      http*://www.google.com/reader*
+// @exclude      http*://docs.google.com/*
+// @exclude      http*://mail.google.com/*
+// @exclude      http*://maps.google*
+// @exclude      http*://www.inoreader.com/*
+// @exclude      http*://www.iqiyi.com/*
+// @exclude      http*://mail.qq.com/*
+// @exclude      http*://openapi.qzone.qq.com/*
+// @exclude      http*://w.qq.com/*
+// @exclude      http*://web2.qq.com/*
+// @exclude      http*://www.toodledo.com/*
+// @exclude      http*://weibo.com/*
+// @exclude      http*://www.wumii.com/*
+// @exclude      http*://*cloud.vip.xunlei.com/*
+// @exclude      http*://app.yinxiang.com/*
+// @exclude      http*://www.youku.com/*
+// @exclude      http*://v.youku.com/*
+// @exclude      http*://*zhihu.com/*
 // ==/UserScript==
 
 
 // 主要用于 chrome 原生下检查更新，也可用于手动检查更新
 var scriptInfo = {
-    version: '6.5.1',
-    updateTime: '2017/8/20',
+    version: '6.5.2',
+    updateTime: '2017/8/22',
     homepageURL: 'https://greasyfork.org/scripts/293-super-preloaderplus-one',
     downloadUrl: 'https://greasyfork.org/scripts/293-super-preloaderplus-one/code/Super_preloaderPlus_one.user.js',
     metaUrl: 'https://greasyfork.org/scripts/293-super-preloaderplus-one/code/Super_preloaderPlus_one.meta.js',
