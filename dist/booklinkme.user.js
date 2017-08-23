49e741203010146ac03d2dd16fc65e5d3cf8648b
greatYear <ywzhaiqi@gmail.com>
2017-08-24 01:50:58 +0800

    拆分 booklinkme

--- dist/booklinkme.user.js
+++ dist/booklinkme.user.js
@@ -1,7 +1,10 @@
+(function () {
+'use strict';
+
 // ==UserScript==
 // @id             booklinkme@ywzhaiqi@gmail.com
 // @name           My booklink.me 增强
-// @version        2.0
+// @version        2.1
 // @author         ywzhaiqi@gmail.com
 // @description    首页一键打开所有未读链接
 // @include        *://booklink.me/*
@@ -15,25 +18,68 @@
 // @require https://greasyfork.org/scripts/32445-zepto-js-selector/code/Zeptojs%20+%20selector.js?version=212976
 // ==/UserScript==
 
-import $ from 'zepto'
-import { isFirefox, GM_request, $x } from './utils'
-import './booklinkme.css'
+var style = "\r\n  .mclicked {\r\n    color: #666666 !important;\r\n  }";
 
-const config = {
-  clickedColor: "666666",
-};
+// 参考 https://github.com/madrobby/zepto/blob/master/src/detect.js
 
-// booklink.me 隐藏符合文字的链接
-function hideChapterLink(selector, textReg) {
-  var links = document.querySelectorAll(selector),
-    link;
-  for (var i = links.length - 1; i >= 0; i--) {
-    link = links[i];
-    if (textReg.test(link.innerHTML)) {
-      link.style.color = "rgba(128, 128, 128, 0.54)";
-    }
-  }
+const ua = navigator.userAgent;
+const platform = navigator.platform;
+
+const isFirefox = na.match(/Firefox\/([\d.]+)/);
+
+const isChrome = ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/);
+
+const isWindows = /Win\d{2}|Windows/.test(platform);
+
+/**
+ * GM_xmlhttpRequest Promise 版
+ *
+ * @export
+ * @param {string} url
+ * @param {object} [opt={}]
+ * @returns {Promise<string>}
+ */
+function GM_request(url, opt = {}) {
+    return new Promise((resolve, reject) => {
+        opt = Object.assign(opt, {
+            method: "GET",
+            url,
+            onload: function (response) {
+                resolve(response.responseText);
+            }
+        });
+
+        GM_xmlhttpRequest(opt);
+    });
 }
+
+/**
+ * 根据 xpath 查找元素
+ *
+ * @export
+ * @param {string} aXPath
+ * @param {Node} aContext
+ * @returns
+ */
+function $x(aXPath, aContext) {
+    var nodes = [];
+    var doc = document;
+    var aContext = aContext || doc;
+
+    try {
+        var results = doc.evaluate(aXPath, aContext, null,
+            XPathResult.ANY_TYPE, null);
+        var node;
+        while (node = results.iterateNext()) {
+            nodes.push(node);
+        }
+    } catch (ex) { }
+
+    return nodes;
+}
+
+// import $ from 'zepto'
+GM_addStyle(style);
 
 class PcPage {
   init() {
@@ -69,7 +115,7 @@ class PcPage {
     appendElem.appendChild(linkBtn);
     appendElem.style.width = "auto";
 
-    console.info('新增功能：一键打开所有未读链接')
+    console.info('新增功能：一键打开所有未读链接');
   }
   _openAllUnreadLinks(event) {
     event.preventDefault();
@@ -116,18 +162,18 @@ class MobilePage {
   }
 
   setClickColor() {
-    const handleClick = function() {
-      $(this).addClass('mclicked')
-    }
-    const linkBlank = function() {
-      $(this).parent().attr('target', '_blank')
-    }
+    const handleClick = function () {
+      $(this).addClass('mclicked');
+    };
+    const linkBlank = function () {
+      $(this).parent().attr('target', '_blank');
+    };
 
     $('span:contains(未读)')
       .on('click', handleClick)
-      .each(linkBlank)
+      .each(linkBlank);
 
-    console.info('新增功能："N未读" 链接新标签打开，打开后变色')
+    console.info('新增功能："N未读" 链接新标签打开，打开后变色');
   }
 
   addUnreadButton() {
@@ -138,23 +184,23 @@ class MobilePage {
     if (unReadLinks.length == 0) return;
 
     var openAllBtn = document.createElement("a");
-    openAllBtn.href = "javascript:void(0)"
+    openAllBtn.href = "javascript:void(0)";
     openAllBtn.style.color = "red";
     openAllBtn.innerHTML = "(" + unReadLinks.length + "未读)";
     var openOneLink = function () {
       var link = unReadLinks.pop().parentNode;
       link.setAttribute("target", "_blank");
-      link.click()
+      link.click();
       link.className = "mclicked";
       if (unReadLinks.length == 0) {
-        openAllBtn.parentNode.removeChild(openAllBtn)
+        openAllBtn.parentNode.removeChild(openAllBtn);
       } else {
-        openAllBtn.innerHTML = "(" + unReadLinks.length + "未读)"
+        openAllBtn.innerHTML = "(" + unReadLinks.length + "未读)";
       }
     };
     openAllBtn.addEventListener("click", function () {
       for (var i = 0; i < 4; i++) {
-        openOneLink()
+        openOneLink();
       }
 
       // openAllBtn.style.color = "#666666";
@@ -162,64 +208,66 @@ class MobilePage {
 
     mainElem.appendChild(openAllBtn);
 
-    console.info('新增功能：一键打开所有未读链接')
+    console.info('新增功能：一键打开所有未读链接');
   }
 }
 
-
 class BookPage {
-    init() {
-        switch (location.pathname) {
-            case '/book-1-1009272444.html':  // 维秘女模的经纪人：内容错误
-                // const newUrl = 'http://www.wangshuge.com/books/109/109265/';
-                let newUrl = 'http://www.wutuxs.com/html/7/7221/'
+  init() {
+    let newUrl;
+    switch (location.pathname) {
+      case '/book-1-1009272444.html':  // 维秘女模的经纪人：内容错误
+        // const newUrl = 'http://www.wangshuge.com/books/109/109265/';
+        newUrl = 'http://www.wutuxs.com/html/7/7221/';
 
-                this.insertToFirst(newUrl, '我的补充')
-                console.info('新增：我的补充 链接')
-                break;
-            case '/book-10-3146622.html':  // 重生在跑道上：内容只有乐文是正确的
-                let newIndexUrl = 'http://www.lwxs520.com/books/74/74985/index.html'
+        this.insertToFirst(newUrl, '我的补充');
+        console.info('新增：我的补充 链接');
+        break;
+      case '/book-10-3146622.html':  // 重生在跑道上：内容只有乐文是正确的
+        newUrl = 'http://www.lwxs520.com/books/74/74985/index.html';
 
-                // insertToFirst(newIndexUrl, '乐文目录')
-                this.getAndInsertFirst(newIndexUrl)
-                console.info('新增：我的补充 链接')
-                break;
-        }
+        this.insertToFirst(newUrl, '乐文目录');
+        // this.getAndInsertFirst(newUrl)
+        console.info('新增：我的补充 链接');
+        break;
     }
+  }
 
-    insertToFirst(newUrl, text) {
-        var firstLink = $x('//font[text()="补充链接"]/..')[0];
-        firstLink.insertAdjacentHTML('beforebegin', `
+  insertToFirst(newUrl, text) {
+    var firstLink = $x('//font[text()="补充链接"]/..')[0];
+    firstLink.insertAdjacentHTML('beforebegin', `
         <a href="${newUrl}" target="_blank" style="margin-right: 3px;">
             <font color="red">${text}</font>
-        </a>`)
-        return firstLink
+        </a>`);
+    return firstLink
+  }
+
+  async getAndInsertFirst(url) {
+    let html = await GM_request(url);
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+
+    if (url.includes('www.lwxs520.com')) {
+      var chapters = $x('//div[@class="dccss"]/a', doc);
+      var lastChapter = chapters[chapters.length - 1];
+
+      this.insertToFirst(url, lastChapter.textContent);
     }
-
-    async getAndInsertFirst(url) {
-        let html = await GM_request(url);
-        var doc = new DOMParser().parseFromString(html, 'text/html')
-
-        if (url.includes('www.lwxs520.com')) {
-            var chapters = $x('//div[@class="dccss"]/a', doc);
-            var lastChapter = chapters[chapters.length - 1];
-
-            this.insertToFirst(url, lastChapter.textContent)
-        }
-    }
+  }
 }
 
 function run() {
-    switch (location.hostname) {
-        case "booklink.me":
-            new PcPage().init();
+  switch (location.hostname) {
+    case "booklink.me":
+      new PcPage().init();
 
-            new BookPage().init()
-            break;
-        case "m.booklink.me":
-            new MobilePage().init();
-            break;
-    }
+      new BookPage().init();
+      break;
+    case "m.booklink.me":
+      new MobilePage().init();
+      break;
+  }
 }
 
-run()
+run();
+
+}());
---
+++ src/booklinkme/meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             booklinkme@ywzhaiqi@gmail.com
+// @name           My booklink.me 增强
+// @version        2.1
+// @author         ywzhaiqi@gmail.com
+// @description    首页一键打开所有未读链接
+// @include        *://booklink.me/*
+// @include        *://m.booklink.me/*
+// @grant          GM_openInTab
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @connect        www.lwxs520.com
+// @run-at         document-end
+// @noframes
+// @require https://greasyfork.org/scripts/32445-zepto-js-selector/code/Zeptojs%20+%20selector.js?version=212976
+// ==/UserScript==
