63387770bd25a049cf8aecbc2dfb6ca7f93a8a14
greatYear <ywzhaiqi@gmail.com>
2017-09-07 20:55:46 +0800

    MyNovelReader 6.0.5 更新过滤规则

--- src/MyNovelReader/meta.js
+++ src/MyNovelReader/meta.js
@@ -3,7 +3,7 @@
 // @name           My Novel Reader
 // @name:zh-CN     小说阅读脚本
 // @name:zh-TW     小說閱讀腳本
-// @version        6.0.4
+// @version        6.0.5
 // @namespace      https://github.com/ywzhaiqi
 // @author         ywzhaiqi
 // @contributor    Roger Au, shyangs, JixunMoe、akiba9527 及其他网友
